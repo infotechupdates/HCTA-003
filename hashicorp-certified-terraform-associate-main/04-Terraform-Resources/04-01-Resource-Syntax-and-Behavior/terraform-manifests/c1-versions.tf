c89807f28c3fb4fb0e0ef97cfc0eb7a63940370d
Dhiraj <dksinghsbs@gmail.com>
2023-10-15 03:34:30 +0530

    Adding Terraform HCTA-003 raw files

---
+++ hashicorp-certified-terraform-associate-main/04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c1-versions.tf
@@ -0,0 +1,15 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.5.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.7"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+}
